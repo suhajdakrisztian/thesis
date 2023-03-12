ad2fe08eb6dafeb0e99ee49cf1a4f972ee01bf36
suhajdakrisztian <krisztian.suhajda@evk.hu>
2023-03-12 23:33:35 +0100

    Beta implementation for iota

--- src/p_iota.h
+++ src/p_iota.h
@@ -1,33 +1,44 @@
 #pragma once
 
+#include "Utils.h"
+
 #include <execution>
+#include <future>
 #include <thread>
 #include <iterator>
-#include <future>
+#include <vector>
 
 namespace parallel {
-    template<class ExecutionPolicy, class ForwardIter>
-    void iota(ExecutionPolicy &&exec, ForwardIter first, ForwardIter last, auto n) {
 
-      const auto chunks = std::thread::hardware_concurrency();
-      const unsigned int dist = std::distance(last, first);
-      const auto chunkLen = dist / chunks;
+    template<class ExecutionPolicy, class ForwardIter, class Value>
+    void iota(ExecutionPolicy &&exec, ForwardIter first,
+              ForwardIter last, Value v) {
 
-      std::vector<std::future<void>> tasks(chunks);
+      const auto element_count = std::distance(first, last);
+      const auto task_count = 7; //this should be refined to some actual calculation
+      const auto step_size = element_count / task_count;
 
-      auto current_begin = first;
-      for(auto i = 0; i < chunks; ++ i) {
+      std::vector<std::pair<ForwardIter, ForwardIter>> ranges(task_count);
+      std::vector<Value> numbers(task_count);
 
-        auto startIt = std::next(current_begin, i * chunkLen);
-        auto endIt = std::min(last, std::next(startIt, chunkLen));
+      ForwardIter last_first = first;
+      ForwardIter last_end;
 
-        tasks[i] = std::async(std::launch::async, [&] {
-            std::iota(startIt, endIt,n);
-        });
+      for(auto i = 0ul; i < task_count; ++ i) {
+        numbers[i] = i ? numbers[i - 1] + step_size : v;
+        ranges[i].first = i ? first + i * step_size : first;
+        ranges[i].second = i + 1 == task_count ? last : first + i * step_size + step_size;
       }
-      for(auto& task: tasks) {
-        task.wait();
+      //ranges.back().second = last; //this needed for unequal ranges
+
+      std::vector<std::shared_future<void>> tasks(task_count);
+
+      for(auto i = 0ul; i < task_count; i ++) {
+        tasks[i] = std::async(std::launch::async,
+                              [&]() {
+                                  std::iota(ranges[i].first, ranges[i].second, numbers[i]);
+                              });
       }
+      for(auto &task: tasks) { task.wait(); }
     }
-} // namespace
-
+}  // namespace pstl
