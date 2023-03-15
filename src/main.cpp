3ccc9384191995d3a1473f59b9f8abac99b725ac
suhajdakrisztian <krisztian.suhajda@evk.hu>
2023-03-16 00:36:20 +0100

    Adding working implementation for iota

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,61 @@
-#include "p_copy_if.h"
-#include "p_find_if.h"
-
-#include <execution>
-#include <iostream>
 #include <vector>
+#include "pstl_find_if.h"
+#include "pstl_iota.h"
+#include <chrono>
+#include <cassert>
 
 int main() {
-  std::vector<int> v(100'000, 2);
-  v[5001] = 123;
-  v[11001] = 999;
-  v[22222] = 1233;
-  v[33333] = 546464;
-  v[55555] = 555;
-  v[77777] = 7657;
-  v[77778] = 7658;
-  v[77779] = 7659;
-  v[77777] = 7657;
-  v[88888] = 312231;
+/*
+  std::vector<int> v(100, 2);
+  v[5] = 123;
+  v[12] = 999;
+  v[17] = 1233;
+  v[23] = 546464;
+  v[45] = 555;
+  v[66] = 7657;
+  v[77] = 7658;
+  v[78] = 7659;
+  v[79] = 7655;
+  v[93] = 312231;
 
-  std::vector<int> res;
+  auto res = parallel::find_if(
+          std::execution::par,
+          v.begin(),
+          v.end(),
+          [](int i) { return i > 50; },
+          [counter = 0](int i) mutable { return ++ counter >= 5; });
 
-  parallel::PSTL<std::vector<int>::iterator> p;
-  auto qqq = p.copy_if(
-      std::execution::par, v.begin(), v.end(),std::back_inserter(res), [](int i) { return i > 50; },
-      [counter = 0](int i) mutable { return ++counter >= 5; });
+  std::cout << *res << std::endl;*/
+
+  //std::vector<int> benchmark {- 10, - 9, - 8, - 7, - 6, - 5, - 4, - 3, - 2, - 1};
+  //std::vector<int> vec(15);
+  //parallel::iota(vec.begin(), vec.end(), -20);
+  //assert(benchmark == vec);
+
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration;
+
+  auto tl1 = high_resolution_clock::now();
+  std::vector<int> linear(200'000);
+  std::iota(linear.begin(), linear.end(), 1);
+  auto tl2 = high_resolution_clock::now();
+
+  auto ls_int = duration_cast<std::chrono::milliseconds>(tl2 - tl1);
+  linear.clear();
+
+  auto tp1 = high_resolution_clock::now();
+  std::vector<int> vec(200'000);
+  parallel::iota(vec.begin(), vec.end(), 1);
+  auto tp2 = high_resolution_clock::now();
+
+  auto ps_int = duration_cast<std::chrono::milliseconds>(tp2 - tp1);
+
+  std::cout << ls_int.count() << std::endl << ps_int.count();
+
+  //assert(vec.size() == 100);
+  //assert(vec.front() == 1);
+  //assert(vec.back() == 100);
+  //assert(std::accumulate(vec.begin(), vec.end(), 0) == 5050);
 
   return 0;
 }
--- src/p_iota.h
+++ src/p_iota.h
@@ -7,38 +7,43 @@
 #include <thread>
 #include <iterator>
 #include <vector>
+#include <iostream>
 
 namespace parallel {
 
-    template<class ExecutionPolicy, class ForwardIter, class Value>
-    void iota(ExecutionPolicy &&exec, ForwardIter first,
-              ForwardIter last, Value v) {
+    template<class ForwardIter, class ValueType>
+    void iota(ForwardIter begin, ForwardIter end, ValueType value) {
 
-      const auto element_count = std::distance(first, last);
+      //some smart checks should happen here regarding eligibility for parallel execution
+
+      const auto element_count = std::distance(begin, end);
       const auto task_count = 7; //this should be refined to some actual calculation
       const auto step_size = element_count / task_count;
 
       std::vector<std::pair<ForwardIter, ForwardIter>> ranges(task_count);
-      std::vector<Value> numbers(task_count);
+      std::vector<ValueType> numbers(task_count);
 
-      ForwardIter last_first = first;
+      ForwardIter last_first = begin;
       ForwardIter last_end;
 
-      for(auto i = 0ul; i < task_count; ++ i) {
-        numbers[i] = i ? numbers[i - 1] + step_size : v;
-        ranges[i].first = i ? first + i * step_size : first;
-        ranges[i].second = i + 1 == task_count ? last : first + i * step_size + step_size;
+      for(auto i = 0; i < task_count; ++ i) {
+        numbers[i] = i ? numbers[i - 1] + step_size : value;
+        ranges[i].first = i ? begin + i * step_size : begin;
+        ranges[i].second = i + 1 == task_count ? end : begin + i * step_size + step_size;
       }
-      //ranges.back().second = last; //this needed for unequal ranges
 
-      std::vector<std::shared_future<void>> tasks(task_count);
+      std::vector<std::future<void>> futures(task_count);
 
-      for(auto i = 0ul; i < task_count; i ++) {
-        tasks[i] = std::async(std::launch::async,
-                              [&]() {
-                                  std::iota(ranges[i].first, ranges[i].second, numbers[i]);
-                              });
+      for(auto i = 0; i < task_count; ++ i) {
+        futures[i] = std::async(std::launch::async, [&, value = numbers[i], i]() mutable {
+            for(auto it = ranges[i].first; it < ranges[i].second; it ++) {
+              *it = value++;
+            }
+        });
       }
-      for(auto &task: tasks) { task.wait(); }
+
+      for(auto &future: futures) {
+        future.wait();
+      }
     }
 }  // namespace pstl
